2048ccd03f6af262d0f22a27a66bd157684ca199
Pablo Torres <pablo.torres.t@gmail.com>
2013-07-09 11:50:37 +0100

    new streaming model

--- api.py
+++ api.py
@@ -32,7 +32,55 @@ class ApiStreaming(object):
         iterTweets = self.twitter.statuses.filter(track = track)
         return iterTweets
 
-class Api(object):
+class ApiSearch(object):
+    
+    def __init__(self,
+                    consumer_key,
+                    consumer_secret,
+                    access_token_key,
+                    access_token_secret):
+        self.twitter = Twitter( auth = OAuth(access_token_key, 
+                                             access_token_secret,
+                                             consumer_key,
+                                             consumer_secret))
+        
+    def GetSearch(self,
+                    q = None,
+                    #geocode = None,
+                    #lang = None,
+                    #locale = None,
+                    #result_type = None,
+                    count = None,
+                    #until = None,
+                    #since_id = None,
+                    max_id = None,
+                    #include_entities = None,
+                    #callback = None
+                    ):
+        tweets = self.twitter.search.tweets(q = q,
+                            #geocode = geocode,
+                            #lang = lang,
+                            #locale = locale,
+                            #result_type = result_type,
+                            count = count,
+                            #until = until,
+                            #since_id = since_id,
+                            max_id = max_id,
+                            #include_entities = include_entities,
+                            #callback = callback
+                            )
+
+        tweetsList = toListOfTweets(tweets)
+        return tweetsList
+
+    def GetSearchRateLimit(self):
+        limits = self.twitter.application.rate_limit_status()
+        remaining = limits['resources']['search']['/search/tweets']['remaining']
+        reset = limits['resources']['search']['/search/tweets']['reset']
+        limit = limits['resources']['search']['/search/tweets']['limit']
+        return [remaining, reset, limit]
+
+class ApiSearchFuture(object):
     
     def __init__(self,
                     consumer_key,
--- twitter_db.py
+++ twitter_db.py
@@ -3,11 +3,9 @@ import requests
 import json
 
 class Webservice(object):
-    #TODO: clean and order
     def __init__(self, urlbase):
         self.urlbase = urlbase
         self.urlInsertTweet = urlbase + "/tweets.json"
-        self.urlInsertTweetSearch = urlbase + "/tweetsearches.json"
         self.urlInsertUser = urlbase + "/users.json"
         self.auth = ('quiltro', 'perroCallejero')
 
@@ -17,19 +15,12 @@ class Webservice(object):
         if r.status_code != 201:
             raise Exception("Post user failed, status code = " +
                                 str(r.status_code) + ", message = " + r.text)
-
     def postTweet(self, tweet):
         data = tweet.toHash()
         r = requests.post(self.urlInsertTweet, data, auth = self.auth)
         if r.status_code != 201:
             raise Exception("Post tweet failed, status code = " +
                                 str(r.status_code) + ", message = " + r.text)
-    
-    def postTweetSearch(self, tweet, search_id):
-        data = { "tweet":  tweet.id , "search": search_id }
-        r = requests.post(self.urlInsertTweetSearch, data, auth = self.auth)
-        if r.status_code != 201:
-            raise Exception("Post tweet_search failed, " + "status code = " + str(r.status_code) + ", message = " + r.text)
 
     def headUser(self, user):
         url = self.urlbase + "/users/" + str(user.id) + ".json"
@@ -53,6 +44,63 @@ class Webservice(object):
             raise Exception("Head tweet failed, status code = " +
                                 str(r.status_code))
 
+class WebserviceStreaming(Webservice):
+    def __init__(self, urlbase):
+        super(WebserviceStreaming, self).__init__(urlbase)
+        self.urlInsertTweetStreaming = urlbase + "/tweetstreamings.json"
+
+    def postUser(self, user):
+        return super(WebserviceStreaming, self).postUser(user)
+
+    def postTweet(self, tweet):
+        return super(WebserviceStreaming, self).postTweet(tweet)
+
+    def headUser(self, user):
+        return super(WebserviceStreaming, self).headUser(user)
+
+    def headTweet(self, tweet):
+        return super(WebserviceStreaming, self).headTweet(tweet)
+
+    def postTweetStreaming(self, tweet, streaming_id):
+        data = { "tweet":  tweet.id , "streaming": streaming_id }
+        r = requests.post(self.urlInsertTweetStreaming, data, auth = self.auth)
+        if r.status_code != 201:
+            raise Exception("Post tweet_streaming failed, " + "status code = " +
+                             str(r.status_code) + ", message = " + r.text)
+
+    def getTweetStreamingParams(self, tweet, streaming_id):
+        url = self.urlbase + "/tweetsteamings.json?streaming_id=" + str(streaming_id) + "&tweet_id=" + str(tweet.id)
+        r = requests.get(url, auth = self.auth)
+        if r.status_code == 200:
+            if not r.text == "[]":
+                return True
+        return False
+
+class WebserviceSearch(Webservice):
+    #TODO: clean and order
+    def __init__(self, urlbase):
+        super(WebserviceSearch, self).__init__(urlbase)
+        self.urlInsertTweetSearch = urlbase + "/tweetsearches.json"
+
+    def postUser(self, user):
+        return super(WebserviceSearch, self).postUser(user)
+
+    def postTweet(self, tweet):
+        return super(WebserviceSearch, self).postTweet(tweet)
+
+    def headUser(self, user):
+        return super(WebserviceSearch, self).headUser(user)
+
+    def headTweet(self, tweet):
+        return super(WebserviceSearch, self).headTweet(tweet)
+
+    def postTweetSearch(self, tweet, search_id):
+        data = { "tweet":  tweet.id , "search": search_id }
+        r = requests.post(self.urlInsertTweetSearch, data, auth = self.auth)
+        if r.status_code != 201:
+            raise Exception("Post tweet_search failed, " + "status code = " +
+                            str(r.status_code) + ", message = " + r.text)
+
     def getTweetSearchParams(self, tweet, search_id):
         url = self.urlbase + "/tweetsearches.json?search_id=" + str(search_id) + "&tweet_id=" + str(tweet.id)
         r = requests.get(url, auth = self.auth)
@@ -61,9 +109,30 @@ class Webservice(object):
                 return True
         return False
 
-class WebserviceDb(object):
+class WebserviceDbStreaming(object):
     def __init__(self, urlbase):
-        self.service = Webservice(urlbase)
+        self.service = WebserviceStreaming(urlbase)
+
+    def insertTweet(self, tweet, streaming_id):
+        try:
+            if not self.service.headUser(tweet.user):
+                print("Adding new user id '", tweet.user.id, "'")
+                self.service.postUser(tweet.user)
+
+            if not self.service.headTweet(tweet):
+                print("Expanding new tweet id '", tweet.id, "'")
+                self.service.postTweet(tweet)
+
+            if not self.service.getTweetStreamingParams(tweet, streaming_id):
+                print("Linking tweet id'", tweet.id, "' with current streaming")
+                self.service.postTweetStreaming(tweet, streaming_id)
+        except Exception as e:
+            print("Tweet insertion failed")
+            print(e)
+
+class WebserviceDbSearch(object):
+    def __init__(self, urlbase):
+        self.service = WebserviceSearch(urlbase)
 
     def insertTweet(self, tweet, search_id):
         try:
